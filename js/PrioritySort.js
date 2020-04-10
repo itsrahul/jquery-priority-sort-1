2b1ccf05b24e479821a8f398c2602189c2e2c909
itsrahul <rahul17a1997@gmail.com>
2020-04-10 14:06:44 +0530

    Add files via upload

---
+++ js/PrioritySort.js
@@ -0,0 +1,86 @@
+export default class PrioritySort
+{
+  constructor(options)
+  {
+    this.$listElement           = options.$listElement
+    this.$seeAllElement         = $(options.seeAllId);
+    this.$seeLessElement        = $(options.seeLessId);
+    this.initialCountAttribute  = options.initialCountAttribute;
+    this.priorityOrderAttribute = options.priorityOrderAttribute;
+  }
+
+  init()
+  {
+    this.bindEvents();
+    this.sortByPriority();
+    this.$seeLessElement.toggle();
+    // this.$seeAllElement.toggle();
+  }
+
+  bindEvents()
+  {
+    this.$seeAllElement.on("click", () => this.onClickSeeAll());
+    this.$seeLessElement.on("click", () => this.onClickSeeLess());
+  }
+
+  onClickSeeAll()
+  {
+    this.sortDefault();
+    $(event.target).toggle();
+    this.$seeLessElement.toggle();
+    event.preventDefault();
+
+  }
+
+  onClickSeeLess()
+  {
+    this.sortByPriority();
+    $(event.target).toggle();
+    this.$seeAllElement.toggle();
+    event.preventDefault();
+  }
+
+  sortByPriority()
+  {
+    let count = this.$listElement.attr(this.initialCountAttribute);    
+    let items = Array.prototype
+      .slice.call( this.$listElement.children().hide() )
+      .sort( (a,b) => this.sortArrayFunction(a, b, "priority") )
+      .slice(0, count);
+
+    items.forEach(element => this.$listElement.append($(element).show()) );
+  }
+
+  sortDefault()
+  {
+    let items = Array.prototype
+      .slice.call( this.$listElement.children().hide() )
+      .sort( (a,b) => this.sortArrayFunction(a, b, "default"))
+
+    items.forEach(element => this.$listElement.append($(element).show()) );
+
+  }
+
+  sortArrayFunction(a,b, type)
+  {
+    let orderA;
+    let orderB;
+    if(type == "priority")
+    {
+      orderA = $(a).attr(this.priorityOrderAttribute) || 40;
+      orderB = $(b).attr(this.priorityOrderAttribute) || 40;
+    }
+    else
+    {
+      orderA = $(a).prop("textContent");
+      orderB = $(b).prop("textContent");
+    }
+    if (orderA < orderB) {
+      return -1;
+    }
+    if (orderA > orderB) {
+      return 1;
+    }
+    return 0;
+  }
+}
---
+++ js/sort.js
@@ -0,0 +1,13 @@
+import PrioritySort from "./PrioritySort.js"
+
+$(document).ready(function(){
+  let options  = {
+    $listElement: $(".priority-sort"),
+    seeAllId: "#all",
+    seeLessId: "#less",
+    initialCountAttribute: "data-initial-items-count",
+    priorityOrderAttribute: "data-priority-order",
+  };
+  let sort = new PrioritySort(options);
+  sort.init();
+});
